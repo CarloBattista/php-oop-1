c1ba7441daa80bfeea1413290da6186640d2a3d9
CarloBattista <carlitobatti@gmail.com>
2023-05-31 14:06:11 +0200

    resetto il css + creo variabili d'istanza

--- index.php
+++ index.php
@@ -1,6 +1,11 @@
 <?php
 
-
+class Movie {
+    // Variabili d'istanza
+    public $title;
+    public $genre;
+    public $year;
+}
 
 ?>
 
@@ -11,8 +16,17 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>php-oop-1</title>
+    <!-- CSS -->
+    <link rel="stylesheet" href="./_assets/css/main.css">
 </head>
 <body>
+
+    <div class="container_heading">
+        <h1 class="heading">Movies</h1>
+    </div>
+    <div class="container_contents">
+        <div class="card"></div>
+    </div>
     
 </body>
 </html>
